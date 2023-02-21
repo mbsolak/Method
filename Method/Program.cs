6a9a48f4d5f3bdeeb79ecb1ab948287f51c81825
mustafa <mustafa@DESKTOP-ITHCNH9>
2023-02-21 11:47:36 +0300

    Add project files.

---
+++ Method/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product Elma = new();
+            Elma.Id = 1;
+            Elma.Adi = "Elma";
+            Elma.Fiyati = 2.5;
+            Elma.Aciklama = "Amasya Elması";
+
+            Product Cilek = new();
+            Cilek.Adi = "Çilek";
+            Cilek.Id = 2;
+            Cilek.Fiyati = 5.5;
+            Cilek.Aciklama = "Taze Çilek";
+
+            Product[] UrunList = new Product[] { Elma, Cilek };
+
+            foreach (Product urun in UrunList)
+            {
+                Console.WriteLine(urun);
+            }
+
+            List<int> ss = new List<int> { 1, 2, 3 };
+            List<int> ss1 = new List<int> { 5, 6, 7 };
+            ss = ss1;
+            Console.WriteLine(ss[0]);
+            ss1[0] = 9;
+            Console.WriteLine(ss[0]);
+
+        }
+
+       
+    }
+}
---
+++ Method/SepetManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class SepetManager
+    {
+
+        public void Ekle()
+        {
+            Console.WriteLine("Eklendi");
+        }
+    }
+}
